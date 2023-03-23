b8353ddc5cff47f799ec4ee46f6655b6a5b7e841
Leo Albert <leo.albert.96@web.de>
2023-03-23 14:45:01 +0100

    added initialize funciton for rendering at body  onload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let questions = [
         "answer_3": "Rick & Morty",
         "answer_4": "Barney Stinson",
         "right_answer": 2
-    }
+    },
 
     {
         "question": "Drah di ned um..",
@@ -44,3 +44,90 @@ let questions = [
         "right_answer": 3
     },
 ];
+
+let i = 0;
+
+
+
+
+function init(){
+    showQuestion();
+    showAnswers();
+    showQuestionCount();
+}
+
+function nextQuestion(){
+    if(i == questions.length){
+        i = 0;
+    }else {
+        i++;
+    };
+    init();
+}
+
+function showQuestion(){
+    let questioncontainer = document.getElementById('questioncontainer');
+    let question = questions[i]['question'];
+
+
+    questioncontainer.innerHTML = '';
+    questioncontainer.innerHTML += returnQuestionHTML(question);
+}
+
+function showAnswers(){
+    let answers = document.getElementById('answercontainer');
+    let answer_1 = questions[i]['answer_1'];
+    let answer_2 = questions[i]['answer_2'];
+    let answer_3 = questions[i]['answer_3'];
+    let answer_4 = questions[i]['answer_4'];
+
+    answers.innerHTML = '';
+    answers.innerHTML += returnAnswerHTML(answer_1, answer_2, answer_3, answer_4);
+
+
+}
+
+function showQuestionCount(){
+    let questioncount = document.getElementById('questioncount');
+
+    questioncount.innerHTML = '';
+    questioncount.innerHTML += returnQuestionCountHTML();
+}
+
+function returnQuestionHTML(question){
+    return /*html*/`
+    Frage: ${question}
+    `;
+}
+
+function returnAnswerHTML(answer_1, answer_2, answer_3, answer_4){
+    return /*html*/`
+        <div class="card question-card mb-2">
+                        <div class="card-body">
+                            ${answer_1}
+                        </div>
+                    </div>
+
+                    <div class="card question-card mb-2">
+                        <div class="card-body">
+                            ${answer_2}
+                        </div>
+                    </div>
+
+                    <div class="card question-card mb-2">
+                        <div class="card-body">
+                            ${answer_3}
+                        </div>
+                    </div>
+
+                    <div class="card question-card mb-2">
+                        <div class="card-body">
+                            ${answer_4}
+                        </div>
+                    </div>
+    `;
+}
+
+function returnQuestionCountHTML(){
+    return `Frage <b>${i+1}</b> von <b>${questions.length}</b>`;
+}
