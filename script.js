f39457988983689f925b97a830cd8c493722e400
Leo Albert <leo.albert.96@web.de>
2023-03-23 13:48:18 +0100

    added card and prepared json

---
+++ script.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": "Wer wohnt in einer Ananas ganz tief im Meer?",
+        "answer_1": "Towlie das Handtuch",
+        "answer_2": "Spongebob Schwammkopf",
+        "answer_3": "Rick & Morty",
+        "answer_4": "Barney Stinson",
+        "right_answer": 2
+    }
+
+    {
+        "question": "Drah di ned um..",
+        "answer_1": "die Mädchen ziehn sich um",
+        "answer_2": "das Feuerwerk mach bumm",
+        "answer_3": "Jeannie fährt dich sonst um",
+        "answer_4": "der Kommisar geht um oh oh oh",
+        "right_answer": 4
+    },
+
+    {
+        "question": "Wer ist der Endgegner eines Devs?",
+        "answer_1": "Softwaretester",
+        "answer_2": "Chat GPT",
+        "answer_3": "Larry Page",
+        "answer_4": "Oma's Strickset",
+        "right_answer": 1
+    },
+
+    {
+        "question": "Bubatz bald legal?",
+        "answer_1": "Einstiegsdroge! Nein!",
+        "answer_2": "Fragen Sie doch mal Herrn Aiwanger",
+        "answer_3": "Söder sagt nein!",
+        "answer_4": "2023",
+        "right_answer": 4
+    },
+
+    {
+        "question": "Welcher Job ist der beste?",
+        "answer_1": "Kellner",
+        "answer_2": "CEO",
+        "answer_3": "Software Entwickler",
+        "answer_4": "Feuerwehrmann",
+        "right_answer": 3
+    },
+];
